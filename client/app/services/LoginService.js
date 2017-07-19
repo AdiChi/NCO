71f6a2b2f82011fb6544410014fad0c3ed9896df
Aditi Chirania <P7108648@ntil.com>
2017-07-19 16:09:23 +0530

    NCO-225 Change dashboard chart values to dynamic values

--- client/app/services/LoginService.js
+++ client/app/services/LoginService.js
@@ -16,14 +16,27 @@ function LoginService($filter) {
                             dailySales: '',
                         },
                         chart: {
-                            firstValue: '50,20,10,40,15',
+                            datapoints : [
+                                {"x": "Jul 1", "New York": 231, "Los Angeles": 321, "Chicago": 511},
+                                {"x": "Jul 2", "New York": 110, "Los Angeles": 222, "Chicago": 421},
+                                {"x": "Jul 3", "New York": 321, "Los Angeles": 122, "Chicago": 221},
+                                {"x": "Jul 4", "New York": 432, "Los Angeles": 150, "Chicago": 331},
+                                {"x": "Jul 5", "New York": 111, "Los Angeles": 21, "Chicago": 311}
+                            ],
+                            datacolumns : [
+                                {"id": "New York", "type": "spline"},
+                                {"id": "Los Angeles", "type": "spline"},
+                                {"id": "Chicago", "type": "spline"}
+                            ],
+                            datax : {"id": "x"},
+                            /*firstValue: '50,20,10,40,15',
                             firstColumnName: 'New York',
                             secondValue: '25,15,40,10,20',
                             secondColumnName: 'Los Angeles',
                             thirdValue: '30,20,15,14,30',
                             thirdColumnName: 'Chicago',
-                            templateUrl: 'app/templates/charts/spline.html',
-                            xLabels: 'Jul 1, Jul 2, Jul 3, Jul 4, Jul 5'
+                            xLabels: 'Jul 1, Jul 2, Jul 3, Jul 4, Jul 5',*/
+                            templateUrl: 'app/templates/charts/spline.html'
                         }
 
                     },
@@ -35,14 +48,27 @@ function LoginService($filter) {
                             dailySales: '22'
                         },
                         chart: {
-                            firstValue: '50,20,10,40,15',
+                            datapoints : [
+                                {"x": "Jul 1", "New York": 231, "Los Angeles": 321, "Chicago": 511},
+                                {"x": "Jul 2", "New York": 110, "Los Angeles": 222, "Chicago": 421},
+                                {"x": "Jul 3", "New York": 321, "Los Angeles": 122, "Chicago": 221},
+                                {"x": "Jul 4", "New York": 432, "Los Angeles": 150, "Chicago": 331},
+                                {"x": "Jul 5", "New York": 111, "Los Angeles": 21, "Chicago": 311}
+                            ],
+                            datacolumns : [
+                                {"id": "New York", "type": "bar"},
+                                {"id": "Los Angeles", "type": "bar"},
+                                {"id": "Chicago", "type": "bar"}
+                            ],
+                            datax : {"id": "x"},
+/*                            firstValue: '50,20,10,40,15',
                             firstColumnName: 'New York',
                             secondValue: '25,15,40,10,20',
                             secondColumnName: 'Los Angeles',
                             thirdValue: '30,20,15,14,30',
                             thirdColumnName: 'Chicago',
-                            templateUrl: 'app/templates/charts/bar.html',
-                            xLabels: 'Jul 1, Jul 2, Jul 3, Jul 4, Jul 5'
+                            xLabels: 'Jul 1, Jul 2, Jul 3, Jul 4, Jul 5',*/
+                            templateUrl: 'app/templates/charts/bar.html'
                         }
                     }, {
                         header: 'Popularity',
@@ -52,12 +78,25 @@ function LoginService($filter) {
                             dailySales: ''
                         },
                         chart: {
-                            firstValue: '50,20,10,40,15',
+                            datapoints : [
+                                {"x": "Jul 1", "New York": 231, "Los Angeles": 321, "Chicago": 511},
+                                {"x": "Jul 2", "New York": 110, "Los Angeles": 222, "Chicago": 421},
+                                {"x": "Jul 3", "New York": 321, "Los Angeles": 122, "Chicago": 221},
+                                {"x": "Jul 4", "New York": 432, "Los Angeles": 150, "Chicago": 331},
+                                {"x": "Jul 5", "New York": 111, "Los Angeles": 21, "Chicago": 311}
+                            ],
+                            datacolumns : [
+                                {"id": "New York", "type": "donut"},
+                                {"id": "Los Angeles", "type": "donut"},
+                                {"id": "Chicago", "type": "donut"}
+                            ],
+                            datax : {"id": "x"},
+                            /*firstValue: '50,20,10,40,15',
                             firstColumnName: 'New York',
                             secondValue: '25,15,40,10,20',
                             secondColumnName: 'Los Angeles',
                             thirdValue: '30,20,15,14,30',
-                            thirdColumnName: 'Chicago',
+                            thirdColumnName: 'Chicago',*/
                             templateUrl: 'app/templates/charts/donut.html'
                         }
                     }
@@ -86,14 +125,27 @@ function LoginService($filter) {
                             dailySales: ''
                         },
                         chart: {
-                            firstValue: '100,250,120,240,150',
+                            datapoints : [
+                                {"x": "Jul 1", "USA": 100, "UK": 321, "Canada": 221},
+                                {"x": "Jul 2", "USA": 110, "UK": 130, "Canada": 121},
+                                {"x": "Jul 3", "USA": 321, "UK": 140, "Canada": 321},
+                                {"x": "Jul 4", "USA": 130, "UK": 150, "Canada": 421},
+                                {"x": "Jul 5", "USA": 140, "UK": 160, "Canada": 21}
+                            ],
+                            datacolumns : [
+                                {"id": "USA", "type": "bar"},
+                                {"id": "UK", "type": "bar"},
+                                {"id": "Canada", "type": "bar"}
+                            ],
+                            datax : {"id": "x"},
+/*                            firstValue: '100,250,120,240,150',
                             firstColumnName: 'USA',
                             secondValue: '125,150,340,100,200',
                             secondColumnName: 'UK',
                             thirdValue: '300,250,150,154,230',
                             thirdColumnName: 'Canada',
-                            templateUrl: 'app/templates/charts/bar.html',
-                            xLabels: 'Jul 1, Jul 2, Jul 3, Jul 4, Jul 5'
+                            xLabels: 'Jul 1, Jul 2, Jul 3, Jul 4, Jul 5',*/
+                            templateUrl: 'app/templates/charts/bar.html'
                         }
 
                     },
@@ -105,12 +157,25 @@ function LoginService($filter) {
                             dailySales: '34'
                         },
                         chart: {
-                            firstValue: '100,250,120,240,150',
+                            datapoints : [
+                                {"x": "Jul 1", "USA": 100, "UK": 321, "Canada": 221},
+                                {"x": "Jul 2", "USA": 110, "UK": 130, "Canada": 121},
+                                {"x": "Jul 3", "USA": 321, "UK": 140, "Canada": 321},
+                                {"x": "Jul 4", "USA": 130, "UK": 150, "Canada": 421},
+                                {"x": "Jul 5", "USA": 140, "UK": 160, "Canada": 21}
+                            ],
+                            datacolumns : [
+                                {"id": "USA", "type": "donut"},
+                                {"id": "UK", "type": "donut"},
+                                {"id": "Canada", "type": "donut"}
+                            ],
+                            datax : {"id": "x"},
+                            /*firstValue: '100,250,120,240,150',
                             firstColumnName: 'USA',
                             secondValue: '125,150,340,100,200',
                             secondColumnName: 'UK',
                             thirdValue: '300,250,150,154,230',
-                            thirdColumnName: 'Canada',
+                            thirdColumnName: 'Canada',*/
                             templateUrl: 'app/templates/charts/donut.html'
                         }
                     }, {
@@ -121,14 +186,27 @@ function LoginService($filter) {
                             dailySales: ''
                         },
                         chart: {
-                            firstValue: '100,250,120,240,150',
+                            datapoints : [
+                                {"x": "Jul 1", "USA": 100, "UK": 321, "Canada": 221},
+                                {"x": "Jul 2", "USA": 110, "UK": 130, "Canada": 121},
+                                {"x": "Jul 3", "USA": 321, "UK": 140, "Canada": 321},
+                                {"x": "Jul 4", "USA": 130, "UK": 150, "Canada": 421},
+                                {"x": "Jul 5", "USA": 140, "UK": 160, "Canada": 21}
+                            ],
+                            datacolumns : [
+                                {"id": "USA", "type": "spline"},
+                                {"id": "UK", "type": "spline"},
+                                {"id": "Canada", "type": "spline"}
+                            ],
+                            datax : {"id": "x"},
+/*                            firstValue: '100,250,120,240,150',
                             firstColumnName: 'USA',
                             secondValue: '125,150,340,100,200',
                             secondColumnName: 'UK',
                             thirdValue: '300,250,150,154,230',
                             thirdColumnName: 'Canada',
-                            templateUrl: 'app/templates/charts/spline.html',
-                            xLabels: 'Jul 1, Jul 2, Jul 3, Jul 4, Jul 5'
+                            xLabels: 'Jul 1, Jul 2, Jul 3, Jul 4, Jul 5',*/
+                            templateUrl: 'app/templates/charts/spline.html'
                         }
                     }
                 ],
@@ -156,14 +234,27 @@ function LoginService($filter) {
                             dailySales: ''
                         },
                         chart: {
-                            firstValue: '150,200,220,250,100',
+                            datapoints : [
+                                {"x": "Jul 1", "Shape of You": 100, "Starboy": 321, "Closer": 221},
+                                {"x": "Jul 2", "Shape of You": 110, "Starboy": 130, "Closer": 121},
+                                {"x": "Jul 3", "Shape of You": 321, "Starboy": 140, "Closer": 321},
+                                {"x": "Jul 4", "Shape of You": 130, "Starboy": 150, "Closer": 421},
+                                {"x": "Jul 5", "Shape of You": 140, "Starboy": 160, "Closer": 21}
+                            ],
+                            datacolumns : [
+                                {"id": "Shape of You", "type": "donut"},
+                                {"id": "Starboy", "type": "donut"},
+                                {"id": "Closer", "type": "donut"}
+                            ],
+                            datax : {"id": "x"},
+                            /*firstValue: '150,200,220,250,100',
                             firstColumnName: 'Shape of You',
                             secondValue: '150,180,240,120,220',
                             secondColumnName: 'Starboy',
                             thirdValue: '200,350,250,150,220',
                             thirdColumnName: 'Closer',
                             fourthValue: '250,150,100,180,120',
-                            fourthColumnName: 'Be With You',
+                            fourthColumnName: 'Be With You',*/
                             templateUrl: 'app/templates/charts/donut.html'
                         }
 
@@ -176,7 +267,20 @@ function LoginService($filter) {
                             dailySales: '41'
                         },
                         chart: {
-                            firstValue: '150,200,220,250,100',
+                            datapoints : [
+                                {"x": "Jul 1", "Shape of You": 100, "Starboy": 321, "Closer": 221},
+                                {"x": "Jul 2", "Shape of You": 110, "Starboy": 130, "Closer": 121},
+                                {"x": "Jul 3", "Shape of You": 321, "Starboy": 140, "Closer": 321},
+                                {"x": "Jul 4", "Shape of You": 130, "Starboy": 150, "Closer": 421},
+                                {"x": "Jul 5", "Shape of You": 140, "Starboy": 160, "Closer": 21}
+                            ],
+                            datacolumns : [
+                                {"id": "Shape of You", "type": "spline"},
+                                {"id": "Starboy", "type": "spline"},
+                                {"id": "Closer", "type": "spline"}
+                            ],
+                            datax : {"id": "x"},
+/*                            firstValue: '150,200,220,250,100',
                             firstColumnName: 'Shape of You',
                             secondValue: '150,180,240,120,220',
                             secondColumnName: 'Starboy',
@@ -184,8 +288,8 @@ function LoginService($filter) {
                             thirdColumnName: 'Closer',
                             fourthValue: '250,150,100,180,120',
                             fourthColumnName: 'Be With You',
-                            templateUrl: 'app/templates/charts/spline.html',
-                            xLabels: 'Jul 1, Jul 2, Jul 3, Jul 4, Jul 5'
+                            xLabels: 'Jul 1, Jul 2, Jul 3, Jul 4, Jul 5',*/
+                            templateUrl: 'app/templates/charts/spline.html'
                         }
                     }, {
                         header: 'Daily Sales',
@@ -195,7 +299,20 @@ function LoginService($filter) {
                             dailySales: ''
                         },
                         chart: {
-                            firstValue: '150,200,220,250,100',
+                            datapoints : [
+                                {"x": "Jul 1", "Shape of You": 100, "Starboy": 321, "Closer": 221},
+                                {"x": "Jul 2", "Shape of You": 110, "Starboy": 130, "Closer": 121},
+                                {"x": "Jul 3", "Shape of You": 321, "Starboy": 140, "Closer": 321},
+                                {"x": "Jul 4", "Shape of You": 130, "Starboy": 150, "Closer": 421},
+                                {"x": "Jul 5", "Shape of You": 140, "Starboy": 160, "Closer": 21}
+                            ],
+                            datacolumns : [
+                                {"id": "Shape of You", "type": "bar"},
+                                {"id": "Starboy", "type": "bar"},
+                                {"id": "Closer", "type": "bar"}
+                            ],
+                            datax : {"id": "x"},
+/*                            firstValue: '150,200,220,250,100',
                             firstColumnName: 'Shape of You',
                             secondValue: '150,180,240,120,220',
                             secondColumnName: 'Starboy',
@@ -203,8 +320,8 @@ function LoginService($filter) {
                             thirdColumnName: 'Closer',
                             fourthValue: '250,150,100,180,120',
                             fourthColumnName: 'Be With You',
-                            templateUrl: 'app/templates/charts/bar.html',
-                            xLabels: 'Jul 1, Jul 2, Jul 3, Jul 4, Jul 5'
+                            xLabels: 'Jul 1, Jul 2, Jul 3, Jul 4, Jul 5',*/
+                            templateUrl: 'app/templates/charts/bar.html'
                         }
                     }
                 ],
