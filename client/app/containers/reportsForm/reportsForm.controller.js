25bbab769f34668ae2e270859faf775fd44041ba
Aditi Chirania <P7108648@ntil.com>
2017-07-27 12:53:21 +0530

    •	List of reports under "My Reports" needs to be moved under "General Reports"

--- client/app/containers/reportsForm/reportsForm.controller.js
+++ client/app/containers/reportsForm/reportsForm.controller.js
@@ -2,46 +2,6 @@ class ReportsFormController {
     constructor() {
         this.allReports = [
             {
-                reportName: "Percent of retail source streams",
-                sharedReport: "N",
-                lastModified: "08/05/2017",
-                lastRan: "06/05/2017",
-                dataset: "MediaMelt",
-                url: "reports"
-            },
-            {
-                reportName: "4 week trend report",
-                sharedReport: "N",
-                lastModified: "05/05/2017",
-                lastRan: "02/05/2017",
-                dataset: "MediaMelt",
-                url: "reports"
-            },
-            {
-                reportName: "Top songs by source",
-                sharedReport: "N",
-                lastModified: "04/05/2017",
-                lastRan: "07/05/2017",
-                dataset: "Tableau - Historical",
-                url: "reports"
-            },
-            {
-                reportName: "Playlist position",
-                sharedReport: "N",
-                lastModified: "02/04/2017",
-                lastRan: "09/05/2017",
-                dataset: "MediaMelt",
-                url: "reports"
-            },
-            {
-                reportName: "Playlist creators",
-                sharedReport: "N",
-                lastModified: "08/04/2017",
-                lastRan: "08/05/2017",
-                dataset: "Tableau - Historical",
-                url: "reports"
-            },
-            {
                 reportName: "Songs by City Report",
                 sharedReport: "Y",
                 lastModified: "08/09/2017",
@@ -84,13 +44,95 @@ class ReportsFormController {
         ];
         this.displayCollection = [].concat(this.allReports);
 
+        this.myReports = [
+            {
+                reportName: "Percent of retail source streams",
+                sharedReport: "N",
+                lastModified: "08/05/2017",
+                lastRan: "06/05/2017",
+                dataset: "MediaMelt",
+                url: "reports"
+            },
+            {
+                reportName: "4 week trend report",
+                sharedReport: "N",
+                lastModified: "05/05/2017",
+                lastRan: "02/05/2017",
+                dataset: "MediaMelt",
+                url: "reports"
+            },
+            {
+                reportName: "Top songs by source",
+                sharedReport: "N",
+                lastModified: "04/05/2017",
+                lastRan: "07/05/2017",
+                dataset: "Tableau - Historical",
+                url: "reports"
+            },
+            {
+                reportName: "Playlist position",
+                sharedReport: "N",
+                lastModified: "02/04/2017",
+                lastRan: "09/05/2017",
+                dataset: "MediaMelt",
+                url: "reports"
+            },
+            {
+                reportName: "Playlist creators",
+                sharedReport: "N",
+                lastModified: "08/04/2017",
+                lastRan: "08/05/2017",
+                dataset: "Tableau - Historical",
+                url: "reports"
+            },{
+                reportName: "Songs by City Report",
+                sharedReport: "Y",
+                lastModified: "08/09/2017",
+                lastRan: "02/11/2017",
+                dataset: "MediaMelt",
+                url: "reports"
+            },
+            {
+                reportName: "Date over date song comparison report",
+                sharedReport: "N",
+                lastModified: "08/08/2017",
+                lastRan: "02/11/2017",
+                dataset: "MediaMelt",
+                url: "dateOverDateSong"
+            },
+            {
+                reportName: "Songs sales by territory / territory group report",
+                sharedReport: "Y",
+                lastModified: "06/05/2017",
+                lastRan: "12/05/2017",
+                dataset: "Tableau - Historical",
+                url: "reports"
+            },
+            {
+                reportName: "Songs Comparison Date Over Date Report",
+                sharedReport: "Y",
+                lastModified: "08/11/2017",
+                lastRan: "02/11/2017",
+                dataset: "MediaMelt",
+                url: "reports"
+            },
+            {
+                reportName: "Songs by Date Range comparison report",
+                sharedReport: "N",
+                lastModified: "08/09/2017",
+                lastRan: "02/05/2017",
+                dataset: "Tableau - Historical",
+                url: "reports"
+            }
+        ];
+        this.displayCollection2 = [].concat(this.myReports);
         this.toFormat = function(r) {
             return r.map(function(i) {
                 let list = {
-                    "Report Name" : i.reportName,
-                    "Shared Report (Y/N)" : i.sharedReport,
-                    "Last Modified" : i.lastModified,
-                    "Last Ran" : i.lastRan,
+                    "Report Name": i.reportName,
+                    "Shared Report (Y/N)": i.sharedReport,
+                    "Last Modified": i.lastModified,
+                    "Last Ran": i.lastRan,
                     "Dataset": i.dataset
                 };
                 return list;
